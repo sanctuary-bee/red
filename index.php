e072f998311d295b6089423b2c9e7e62bb48484e
Bee <Bee@Bees-MacBook-Pro.local>
2015-08-24 17:05:12 +0100

    html5 video

--- index.php
+++ index.php
@@ -1,8 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html>
+    <head>  
+        <link rel="stylesheet" href="assets/stylesheets/bootstrap.min.css" type="text/css"/>
+        <link rel="stylesheet" href="assets/stylesheets/screen.css" type="text/css"/>
+        <script src="assets/js/bootstrap.min.js"></script>
+    </head>
+<body>
+    
+    <video autoplay="autoplay" muted loop="loop" class="fullvideo hidden-xs">
+        <source src="assets/vids/vid.mp4" type="video/mp4" />
+        <source src="assets/vids/vid.ogg" type="video/ogg" />
+        <source src="assets/vids/vid.webm" type="video/webm" />
+       
+    </video>
+    <div class="thumb-vid hidden-sm hidden-md hidden-lg"></div>
+     <div class="tosser">hello world!</div>
+    <div class="text-div">
+        
+    </div>
+    
+</body>
+</html>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
